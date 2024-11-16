412e6565008031920e8c4b8c95db3131ef45f552
Josiah Adenegan <startnow65@gmail.com>
2024-11-16 17:55:04 +0000

    Update tagging objects exercise of chapter 4

--- chapter4/tagging-objects-using-a-batch-job/exercise.sh
+++ chapter4/tagging-objects-using-a-batch-job/exercise.sh
@@ -1,3 +1,17 @@
+# Set require variables for subsequent commands
 BUCKET_NAME=devopspro-beyond-2
-sed --regexp-extended --expression "s/(.*),(.*)/${BUCKET_NAME},\2/g" manifest.csv
-aws s3 sync . s3://${BUCKET_NAME}
+TEST_FILES_FOLDER=test-files
+
+# Create a new folder to hold the test files you will upload to the s3 bucket
+mkdir ${TEST_FILES_FOLDER}
+
+# Generate test files
+for i in {1..20}; do
+  str="$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 16)"
+  fileName="${TEST_FILES_FOLDER}/${str}.txt"
+  echo ${str} > ${fileName}
+  echo "${BUCKET_NAME},${fileName}" >> ${TEST_FILES_FOLDER}/manifest.csv
+done
+
+# Upload the manifest file and test files to your s3 bucket
+aws s3 sync ${TEST_FILES_FOLDER} s3://${BUCKET_NAME}/${TEST_FILES_FOLDER}
